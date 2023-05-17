57403b4dea4d6b951cee15af8eecf533dc79c2c6
Necoarcgaming <113333071+Necoarcgaming@users.noreply.github.com>
2023-05-17 12:18:00 +0300

    HAHAHHHHHHHHHAHAA

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,13 +6,7 @@ import "firebase/compat/database";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAiM-k61s_4o9k9za6Xy4LOaA54VRc9N44",
-  authDomain: "todolistabasebased.firebaseapp.com",
-  databaseURL: "https://todolistabasebased-default-rtdb.firebaseio.com/",
-  projectId: "todolistabasebased",
-  storageBucket: "todolistabasebased.appspot.com",
-  messagingSenderId: "305697639549",
-  appId: "1:305697639549:web:9096420beddbdaf06f7514"
+  //Your own firebase config
 };
 
 if (!firebase.apps.length) {
